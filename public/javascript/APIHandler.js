c42c01e08325a568b9f883ba663a6f5a19ad797c
Adrian Ramirez <ramiire@gmail.com>
2021-11-16 21:46:15 +0100

    2 iteracion

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -3,23 +3,28 @@ class APIHandler {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
 
+// SEARCH BUTTON FULL LIST
+
+
+
+  getFullList () {
+    return axios.get("https://minions-api.herokuapp.com/characters")
   }
 
   getOneRegister () {
-
+    return axios.get("https://minions-api.herokuapp.com/characters/:id")
   }
 
   createOneRegister () {
-
+    return axios.get("https://minions-api.herokuapp.com/characters")
   }
 
   updateOneRegister () {
-
+    return axios.get("https://minions-api.herokuapp.com/characters/:id")
   }
 
   deleteOneRegister () {
-
+    return axios.get("https://minions-api.herokuapp.com/characters/:id")
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,12 +1,59 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
+
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
+
+    charactersAPI.getFullList()
+      .then(res => {
+        const charactersList = document.querySelector(".characters-container")
+
+        let charactersInfo = ""
+
+        res.data.reverse().forEach(character => {
+            charactersInfo += `<div class="character-info">
+            
+            <div class="name">Name: ${character.name}</div> </br>
+            <div class="occupation">Occupation: ${character.occupation}</div> </br>
+            <div class="cartoon">Cartoon: ${character.cartoon}</div> </br>
+            <div class="weapon">Weapon: ${character.weapon}</div> </br>
+            
+            </div>`
+          
+        });
+
+        charactersList.innerHTML = charactersInfo
+       
+      })
+      .catch(err => console.log(err))
+
+
+      
+
 
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
 
+    const characterId = document.getElementById("eligeUno").value
+    
+    charactersAPI.getOneRegister(characterId)
+        .then(res => {
+        const charactersList = document.querySelector(".character-info")
+        console.log(res)
+
+        const character = res.data
+
+        let charactersInfo = `
+            <div class="name">${character.name}</div>
+            <div class="occupation">${res.data.occupation}</div>
+            <div class="cartoon">${res.data.cartoon}</div>
+            <div class="weapon">${res.data.weapon}</div>`
+
+
+        charactersList.innerHTML = charactersInfo
+      })
+      .catch(err => console.log(err))
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
@@ -21,3 +68,18 @@ window.addEventListener('load', () => {
 
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
